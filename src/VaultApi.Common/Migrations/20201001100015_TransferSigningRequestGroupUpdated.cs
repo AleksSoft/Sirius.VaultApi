e43dfe0aebd7f7a23fa430fcb9eac4fc81485079
Aleksandr Belousov <aleksandr.belousov@swisschain.io>
2020-10-01 14:00:41 +0400

    fixed

---
+++ src/VaultApi.Common/Migrations/20201001100015_TransferSigningRequestGroupUpdated.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VaultApi.Common.Migrations
+{
+    public partial class TransferSigningRequestGroupUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Group",
+                schema: "vault_api",
+                table: "transaction_signing_requests",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Group",
+                schema: "vault_api",
+                table: "transaction_signing_requests");
+        }
+    }
+}
