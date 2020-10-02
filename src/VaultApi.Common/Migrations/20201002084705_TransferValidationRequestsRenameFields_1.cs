b4460cbd13bf9774d845c0a66164454cde298625
Aleksandr Belousov <aleksandr.belousov@swisschain.io>
2020-10-02 12:47:16 +0400

    fixed

---
+++ src/VaultApi.Common/Migrations/20201002084705_TransferValidationRequestsRenameFields_1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VaultApi.Common.Migrations
+{
+    public partial class TransferValidationRequestsRenameFields_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RejectionReasonString",
+                schema: "vault_api",
+                table: "transfer_validation_requests");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RejectionReasonMessage",
+                schema: "vault_api",
+                table: "transfer_validation_requests",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RejectionReasonMessage",
+                schema: "vault_api",
+                table: "transfer_validation_requests");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RejectionReasonString",
+                schema: "vault_api",
+                table: "transfer_validation_requests",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
